4596eaedf21ae5fe875f08c1580a406138dbea2a
AldoConforto <aldo.conforto@gmail.com>
2023-04-05 23:25:09 +0200

    updated all internal urls

--- media/canonical_twitter_facebook.js
+++ media/canonical_twitter_facebook.js
@@ -7,25 +7,23 @@
 */
 
 const titoloPagina = document.title;
-const urlPagina = document.baseURI
+const urlPagina = window.location.href
 const descrizionePagina = document.querySelector("meta[name='description']").getAttribute("content");
 
-console.log(urlPagina);
-
 let canonicalTwitterFacebook = `
-<link rel="canonical" href="https://aldoconforto.github.io/teatro/index.htm" />
+<link rel="canonical" href="`+urlPagina+`" />
 <meta name="twitter:title" content="`+titoloPagina+`">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="">
 <meta name="twitter:creator" content="">
-<meta name="twitter:url" content="https://aldoconforto.github.io/teatro/">
+<meta name="twitter:url" content="`+urlPagina+`">
 <meta name="twitter:description" content="`+descrizionePagina+`">
 <meta name="twitter:image" content="https://aldoconforto.github.io/teatro/images/Aldo%20Conforto%20vestito%20di%20scena.jpg">
 <meta name="twitter:image:alt" content="Aldo Conforto vestito di scena">
 <meta property="og:title" content="`+titoloPagina+`">
 <meta property="og:type" content="article">
 <meta property="og:email" content="">
-<meta property="og:url" content="https://aldoconforto.github.io/teatro/">
+<meta property="og:url" content="`+urlPagina+`">
 <meta property="og:image" content="https://aldoconforto.github.io/teatro/images/Aldo%20Conforto%20vestito%20di%20scena.jpg">
 <meta property="og:image:alt" content="Aldo Conforto vestito di scena">
 <meta property="og:site_name" content="Aldo Conforto attore regista di teatro">
