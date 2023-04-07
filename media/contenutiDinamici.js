ac4be0eb1c5b70c08982907bd0a641b136a3dbbc
AldoConforto <aldo.conforto@gmail.com>
2023-04-07 09:53:44 +0200

    update

--- media/contenutiDinamici.js
+++ media/contenutiDinamici.js
@@ -273,7 +273,7 @@ function aggiornaContenuto(campoNellaPagina,nuovoContenuto) {
     campoNellaPagina.innerHTML = nuovoContenuto;
 }
 
-async function aggiungiContenutoPrima(campoNellaPagina,contenutoAttuale,contenutoInAggiuntaPrima) {
+function aggiungiContenutoPrima(campoNellaPagina,contenutoAttuale,contenutoInAggiuntaPrima) {
     //campoNellaPagina[0].innerHTML = contenutoInAggiuntaPrima + contenutoAttuale;
     campoNellaPagina.innerHTML = contenutoInAggiuntaPrima + contenutoAttuale;
 }
